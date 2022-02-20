9ee07d46018ac2cdcb0f8ec40ed814b77b0f1517
Michael Sieler <sielerjm@oregonstate.edu>
2022-02-19 19:25:30 -0800

    update start screen, click to start flashes

---
+++ VirtualFish/Assets/Scripts/menuManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class menuManager : MonoBehaviour
+{
+    public GameObject flashText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("flashTheText", 0f, 0.5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonUp(0))
+            SceneManager.LoadScene("Game");
+
+    }
+
+    void flashTheText() {
+        if (flashText.activeInHierarchy)
+            flashText.SetActive(false);
+        else
+            flashText.SetActive(true);
+
+    }
+}
